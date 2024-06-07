c5be4282dca3686ba609b0e4ae2a5a5b1fa56f44
Fábio Milagres <fabioemanuellmilagres@gmail.com>
2024-06-06 23:03:02 -0300

    fix: Enable create post button only on homepage

--- src/components/navbar/navbar-login-menu.tsx
+++ src/components/navbar/navbar-login-menu.tsx
@@ -7,6 +7,7 @@ import { Send, SquarePen } from "lucide-react";
 import { User } from "next-auth";
 import dynamic from "next/dynamic";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useRef, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import "react-quill/dist/quill.snow.css";
@@ -63,6 +64,8 @@ export default function NavbarLoginMenu({
   newPost,
 }: NavbarLoginMenuProps) {
   const queryClient = useQueryClient();
+
+  const pathname = usePathname();
 
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
@@ -149,23 +152,27 @@ export default function NavbarLoginMenu({
           </DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuGroup>
-            <DropdownMenuItem>
-              <DialogTrigger
-                onClick={() => {
-                  form.setValue(
-                    "post-author",
-                    session.userId?.toString() || "128875797"
-                  );
-                }}
-                asChild
-              >
-                <button className="flex items-center cursor-default">
-                  <SquarePen className="mr-2 h-4 w-4" />
-                  <span>Create post</span>
-                </button>
-              </DialogTrigger>
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
+            {pathname === "/" && (
+              <>
+                <DropdownMenuItem>
+                  <DialogTrigger
+                    onClick={() => {
+                      form.setValue(
+                        "post-author",
+                        session.userId?.toString() || "128875797"
+                      );
+                    }}
+                    asChild
+                  >
+                    <button className="flex items-center cursor-default">
+                      <SquarePen className="mr-2 h-4 w-4" />
+                      <span>Create post</span>
+                    </button>
+                  </DialogTrigger>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+              </>
+            )}
             <DropdownMenuItem asChild>
               <Link href={"/profile/posts"}>
                 <Send className="mr-2 h-4 w-4" />
@@ -179,97 +186,99 @@ export default function NavbarLoginMenu({
           </DropdownMenuGroup>
         </DropdownMenuContent>
       </DropdownMenu>
-      <DialogContent className="max-w-4xl w-full">
-        <DialogHeader>
-          <DialogTitle>
-            <p className="flex items-center justify-center md:justify-normal">
-              <SquarePen className="mr-1.5 h-5 w-5" />
-              Create a new post.
-            </p>
-          </DialogTitle>
-          <DialogDescription className="text-center md:text-left">
-            Create a new post to share with the community.
-          </DialogDescription>
-        </DialogHeader>
-        <Form {...form}>
-          <form
-            ref={ref}
-            className="space-y-5"
-            onSubmit={form.handleSubmit(onSubmit)}
-          >
-            <FormField
-              control={form.control}
-              name="post-title"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-right">Post Name</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="text"
-                      placeholder="Your post name."
-                      className="col-span-3"
-                      autoComplete="off"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="post-body"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-right">Post Body</FormLabel>
-                  <FormControl className="col-span-4 max-h-80 h-64">
-                    <Controller
-                      control={form.control}
-                      name="post-body"
-                      render={({ field }) => (
-                        <div className="md:max-w-[52.8rem] max-w-[19.4rem]">
-                          <ReactQuill
-                            value={field.value || ""}
-                            onChange={field.onChange}
-                            className="max-h-80 overflow-y-auto h-64 w-full bg-none shadow-lg"
-                            placeholder="Write your post here..."
-                            modules={quillModules}
-                            formats={quillFormats}
-                            style={{
-                              wordWrap: "break-word",
-                              overflowWrap: "break-word",
-                            }}
-                          />
-                        </div>
-                      )}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="post-author"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl className="hidden">
-                    <Input type="hidden" disabled {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <DialogFooter className="gap-y-2">
-              <DialogClose asChild>
-                <Button type="button" variant={"outline"}>
-                  Cancel
+      {pathname === "/" && (
+        <DialogContent className="max-w-4xl w-full">
+          <DialogHeader>
+            <DialogTitle>
+              <p className="flex items-center justify-center md:justify-normal">
+                <SquarePen className="mr-1.5 h-5 w-5" />
+                Create a new post.
+              </p>
+            </DialogTitle>
+            <DialogDescription className="text-center md:text-left">
+              Create a new post to share with the community.
+            </DialogDescription>
+          </DialogHeader>
+          <Form {...form}>
+            <form
+              ref={ref}
+              className="space-y-5"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="post-title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-right">Post Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        placeholder="Your post name."
+                        className="col-span-3"
+                        autoComplete="off"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="post-body"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-right">Post Body</FormLabel>
+                    <FormControl className="col-span-4 max-h-80 h-64">
+                      <Controller
+                        control={form.control}
+                        name="post-body"
+                        render={({ field }) => (
+                          <div className="md:max-w-[52.8rem] max-w-[19.4rem]">
+                            <ReactQuill
+                              value={field.value || ""}
+                              onChange={field.onChange}
+                              className="max-h-80 overflow-y-auto h-64 w-full bg-none shadow-lg"
+                              placeholder="Write your post here..."
+                              modules={quillModules}
+                              formats={quillFormats}
+                              style={{
+                                wordWrap: "break-word",
+                                overflowWrap: "break-word",
+                              }}
+                            />
+                          </div>
+                        )}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="post-author"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl className="hidden">
+                      <Input type="hidden" disabled {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <DialogFooter className="gap-y-2">
+                <DialogClose asChild>
+                  <Button type="button" variant={"outline"}>
+                    Cancel
+                  </Button>
+                </DialogClose>
+                <Button type="submit" disabled={form.formState.isSubmitting}>
+                  {form.formState.isSubmitting ? "Posting..." : "Post"}
                 </Button>
-              </DialogClose>
-              <Button type="submit" disabled={form.formState.isSubmitting}>
-                {form.formState.isSubmitting ? "Posting..." : "Post"}
-              </Button>
-            </DialogFooter>
-          </form>
-        </Form>
-      </DialogContent>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      )}
     </Dialog>
   );
 }
